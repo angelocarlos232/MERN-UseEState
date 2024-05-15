daf381b45fd7a483d8fcfbb7bf09de1bf7332f55
angelocarlos232 <angelocarlos232@gmail.com>
2024-05-15 16:46:15 +0800

    connect to database and mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Mongodb")
+}).catch(() => {
+    console.log("Mongodb not connected")
+})
 
 const app = express();
 
