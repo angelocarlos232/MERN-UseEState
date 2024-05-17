95eb3be69bbf295a07d49d316b4d10e8efa40b33
angelocarlos232 <angelocarlos232@gmail.com>
2024-05-17 15:46:32 +0800

    created a middleware to handle errors

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import bcryptjs from 'bcrypt'
 
 
 
-export const signup = async (req,res) => {
+export const signup = async (req,res, next) => {
 
     const {username, email, password} = req.body;
 
@@ -15,7 +15,6 @@ export const signup = async (req,res) => {
         await newUser.save();
         res.status(201).json("User created successfully")
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
-        
 }
